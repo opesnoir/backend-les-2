79822d6531eba691567d2efc3082c777ab040847
opesnoir <goodsurroundsme@outlook.com>
2023-03-06 10:51:40 +0100

    huiswerkopdracht afgerond en nagekeken

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,47 +5,53 @@ public class Main {
         int x = 3;
 
         // Pas deze assignement aan zodat variable `four` de waarde `4` toegewezen krijgt.
-        int four = x;
+        int four = x + 1;
         // Pas deze assignement aan zodat variable `one` de waarde `1` toegewezen krijgt.
-        int one = x;
+        int one = x - 2;
         // Pas deze assignement aan zodat variable `three_point_5` de float(!) waarde `3.5` toegewezen krijgt.
-        float three_point_5 = x;
+        float three_point_5 = x + 0.5f;
         // Pas deze assignement aan zodat variable `one_point_5` de waarde `1.5` toegewezen krijgt.
-        double one_point_5 = x;
+        double one_point_5 = x - 1.5;
         // Pas deze assignement aan zodat variable `zero` de waarde `0` toegewezen krijgt. Je mag geen +, -, * of / gebruiken.
+        x = 0;
         int zero = x;
-
         // Wat kunnen we op de lege regel hieronder schrijven zodat de System.out.println daaronder klopt?
-
+        x = 6;
         System.out.println(x); // six
         // Pas onderstaand statement aan zodat dat statement en het statement daarna kloppen
         System.out.println(x); // six
+        x = 7;
         System.out.println(x); // seven
         // Pas onderstaan statement aan zodat het statement klopt.
+        x = 6;
         System.out.println(x); // six
 
         int left = 5;
         int right = 0;
         boolean bool = false;
-        // voeg een assignment toe voor de variabele `right` zodat boolean `a1` true is.
 
+        //a1 voeg een assignment toe voor de variabele `right` zodat boolean `a1` true is.
+        right = 7;
         boolean a1 = left < right;
-        // voeg een assignment toe voor de variabele `right` zodat boolean `a2` true is.
 
+        //a2 voeg een assignment toe voor de variabele `right` zodat boolean `a2` true is.
+        right = 3;
         boolean a2 = left > right;
-        // voeg een assignment toe voor de variabele `right` zodat boolean `a3` true is.
 
+        //a3 voeg een assignment toe voor de variabele `right` zodat boolean `a3` true is.
+        right = 5;
         boolean a3 = left == right;
-        // voeg een assignment toe voor de variabele `bool` zodat boolean `a4` true is.
 
+        //a4 voeg een assignment toe voor de variabele `bool` zodat boolean `a4` true is.
+        bool = true;
         boolean a4 = left <= 3 || bool;
-        // voeg een assignment toe voor de variabele `bool` zodat boolean `a5` true is.
 
+        //a5 voeg een assignment toe voor de variabele `bool` zodat boolean `a5` true is.
+        bool = false;
         boolean a5 = left > 4 && !bool;
 
 
         // Pas geen code onder deze regel aan
-
         System.out.println(four);
         System.out.println(one);
         System.out.println(three_point_5);
